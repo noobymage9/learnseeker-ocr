3a54f47f0a39d2a0fb64e6a31f571041e00e7f5b
Terry Seah <terryseah009@gmail.com>
2021-04-13 14:04:54 +0800

    Refactored image preprocess factory Added phrase capture

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from base64 import b64decode, b64encode
 from os import environ
-from image_preprocess_factory import preprocess, zone
+from ocr import image_preprocess_factory
 from utilities import spell_check
 import numpy as np
 import cv2 as cv
 import pytesseract
+from PIL import Image
+from uuid import uuid4
 app = Flask(__name__)
 
+CAPTURE_PHRASE = environ.get('CAPTURE_PHRASE') == 'True'
 
 @app.route('/home')
 def home():
@@ -38,18 +41,18 @@ def image_preprocess():
 		image_data = image_data_pack.split(',')[1]
 		image_bytes = b64decode(image_data)
 		image = cv.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
-		preprocessed_image = preprocess(image)
+		preprocessed_image = image_preprocess_factory.preprocess(image)
 		data['images'].append(encode(image = preprocessed_image))
 		# if auto bound
 		if 'zone_type' in request.json:
-			cropped_areas = zone(preprocessed_image, request.json['zone_type'])
+			cropped_areas = image_preprocess_factory.zone(preprocessed_image, request.json['zone_type'])
 			for idx, cropped_area in enumerate(cropped_areas):
 				data["texts"].append(pytesseract.image_to_string(cropped_area))
 
 			# spell_check(data['texts'])
 			data['cropped_areas']['areas'].append(encode(images = cropped_areas))
 			data['cropped_areas']['size'].append(list(map(lambda area: [(area.shape[1] / image.shape[1]) * 600, (area.shape[0] / image.shape[0]) * 800], cropped_areas)))
-	
+ 
 	return jsonify(data)
 
 @app.route('/text_recognise', methods=['POST'])
@@ -62,6 +65,11 @@ def text_recognise():
 		image_bytes = b64decode(image_data)
 		image = cv.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
 		data["texts"].append(pytesseract.image_to_string(image))
+
+		if CAPTURE_PHRASE:
+			word_image = image
+			word_image = Image.fromarray(word_image)
+			word_image.save(''.join(['./dataset/phrases', '/', f'{uuid4().hex}.png']), 'PNG')
 
 	# spell_check(data['texts'])
 	return jsonify(data)
