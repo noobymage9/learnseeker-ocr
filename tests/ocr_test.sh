9c3ed3353e6bc0f62fd8551d748f5211868726e3
Terry Seah <terryseah009@gmail.com>
2021-04-13 14:11:20 +0800

    Added ocr test

---
+++ tests/ocr_test.sh
@@ -0,0 +1,8 @@
+echo "Running character accuracy..."
+./scripts/ocreval/character_accuracy.sh
+
+echo "Running word accuracy..."
+./scripts/ocreval/word_accuracy.sh
+
+echo "Running accuracy by count..."
+./scripts/ocreval/accuracy_by_count.sh
