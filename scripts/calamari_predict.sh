acfcebdf0c5293ff95e694a38e09175f6f0abd16
Terry Seah <terryseah009@gmail.com>
2021-04-13 14:08:13 +0800

    Added calamari prediction script

---
+++ scripts/calamari_predict.sh
@@ -0,0 +1,5 @@
+calamari-predict --checkpoint ./assets/calamari_models/0.ckpt --files ./dataset/phrases/* --output_dir ./dataset/recognitions/calamari_0/
+calamari-predict --checkpoint ./assets/calamari_models/1.ckpt --files ./dataset/phrases/* --output_dir ./dataset/recognitions/calamari_1/
+calamari-predict --checkpoint ./assets/calamari_models/2.ckpt --files ./dataset/phrases/* --output_dir ./dataset/recognitions/calamari_2/
+calamari-predict --checkpoint ./assets/calamari_models/3.ckpt --files ./dataset/phrases/* --output_dir ./dataset/recognitions/calamari_3/
+calamari-predict --checkpoint ./assets/calamari_models/4.ckpt --files ./dataset/phrases/* --output_dir ./dataset/recognitions/calamari_4/
